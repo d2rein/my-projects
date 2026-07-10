df622be927baec6b08e96049de734dcbc763f5b1
d2rein <d2.rein@gmail.com>
2026-07-10 20:35:49 +1000

    Lock DND pages to phone viewport width

--- dnd-bladesinger-pwa/sw.js
+++ dnd-bladesinger-pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bladesinger-pwa-v9";
+const CACHE_NAME = "bladesinger-pwa-v10";
 const ASSETS = [
   "./",
   "./index.html",
--- grug/sw.js
+++ grug/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jefferson-grug-v9";
+const CACHE_NAME = "jefferson-grug-v10";
 const ASSETS = [
   "./",
   "./index.html",
